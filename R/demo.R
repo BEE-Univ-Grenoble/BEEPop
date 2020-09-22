a1e3bb9b9ce83e26e8ea689087250e89109affb1
Eric Coissac <eric.coissac@metabarcoding.org>
2020-09-22 08:39:55 +0200

    Initial commit of the BEEPop project

---
+++ R/demo.R
@@ -0,0 +1,15 @@
+#' Somme de deux nombres
+#'
+#' Cette fonction réalise la somme de deux nombres.
+#'
+#' @param a le premier nombre à additionner
+#' @param b le second nombre à additionner
+#'
+#' @return une valeur numerique
+#' @export
+#'
+#' @examples
+#'    foo(3,4)
+foo <- function(a,b) {
+  a + b
+}
