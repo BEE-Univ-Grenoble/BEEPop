ebeb0e25305eb1f2cab67b65bddbd5b5f2272641
Eric Coissac <eric.coissac@metabarcoding.org>
2020-09-22 11:33:38 +0200

    Ajout de la fonction eric qui calcule le double d'un nombre

---
+++ R/eric.R
@@ -0,0 +1,12 @@
+#' Calcule le double d'un nombre
+#'
+#' @param x le nombre dont on veut le double
+#'
+#' @return une valeur numérique
+#' @export
+#'
+#' @examples
+#'   eric(4)
+eric <- function(x) {
+  2 * x
+}
